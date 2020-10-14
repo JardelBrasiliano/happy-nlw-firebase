7614e81c835d638f2bc84246231ede59484b6888
Jardel <jardelbrasiliano@gmail.com>
2020-10-14 16:06:32 -0300

    map e rotas

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiArrowRight } from 'react-icons/fi'
+
+import logoImg from '../images/Logo.svg';
+
+import '../styles/pages/landing.css';
+
+function Landing() {
+    return(
+        <div className="content-wrapper">
+            <img src={logoImg} alt=""/>
+
+            <main>
+            <h1>Leve felicidade para o mundo</h1>
+            <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+            </main>
+
+            <div className="location">
+            <strong>Caucaia</strong>
+            <span>Ceará</span>
+            </div>
+
+            <Link to="/orphanagemap" className="enter-app">
+                <FiArrowRight size={26} color="rgba(0,0,0,0.6)"/>
+            </Link>
+        </div>
+    );
+}
+
+export default Landing;
